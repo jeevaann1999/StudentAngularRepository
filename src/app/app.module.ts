2b4691114712d871bceade7d079aeb94ef997d06
jeevaann1999 <jeevaann1999@gmail.com>
2022-11-29 07:04:37 +0530

    added nav

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StudentEntryComponent } from './student-entry/student-entry.component';
-import { StudentViewComponent } from './student-view/student-view.component';
 import { StudentSearchComponent } from './student-search/student-search.component';
+import { RouterModule, Routes } from '@angular/router';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const myRoute:Routes=[
+  {
+    path:"",
+    component:StudentEntryComponent
+  },
+  {
+    path:"search",
+    component:StudentSearchComponent
+  }
+]
+
 
 @NgModule({
   declarations: [
     AppComponent,
     StudentEntryComponent,
-    StudentViewComponent,
-    StudentSearchComponent
+    StudentSearchComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myRoute)
   ],
   providers: [],
   bootstrap: [AppComponent]
